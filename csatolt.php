6e317bd9fc70e933d75e12ab160c09e021a29c9b
Máté Török <torokmate676@gmail.com>
2023-04-25 18:25:03 +0200

    torles

--- csatolt.php
+++
@@ -1,7 +0,0 @@
-<?php
-$kapcsolat = new mysqli('localhost', 'root', '', 'iphonetokok');
-if(session_status() == PHP_SESSION_NONE){
-    session_start();
-}
-
-?>
--- kepnezo.php
+++
@@ -1,9 +0,0 @@
-<?php
-include './csatolt.php';
-    if(isset($_GET['id'])){
-       $eredmeny = $kapcsolat -> query("select keptipus, kep from termek where id=".$_GET['id']);
-        $sor = $eredmeny -> fetch_array();
-        header("Content-type: ".$sor['keptipus']);
-        echo $sor['kep'];
-    }
-?>
